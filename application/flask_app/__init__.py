388b8c74da057c36c1e531fca321b1c8b6469104
Chris Eckhardt <ceckhardt06@gmail.com>
2020-02-15 11:00:07 -0800

    requirements.txt updated

--- application/flask_app/__init__.py
+++ application/flask_app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import db_addr
+from flask_app.config import db_addr, track_mods
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_addr
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = track_mods
 db = SQLAlchemy(app)
 
 from flask_app import routes
