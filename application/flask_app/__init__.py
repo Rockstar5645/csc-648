3d224ebad8abd434b4f8331b252e727216ccde2b
Chris Eckhardt <ceckhardt06@gmail.com>
2020-02-15 10:16:59 -0800

    config file added to git ignore

--- application/flask_app/__init__.py
+++ application/flask_app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import db_addr
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:test@localhost/db_six'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_addr
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
---
+++ application/flask_app/config.py
@@ -0,0 +1,4 @@
+
+
+db_addr = 'mysql://root:test@localhost/db_six'
+
