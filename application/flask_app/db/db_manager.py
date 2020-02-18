494d43116d9c4b5b8977fe39b8fcbcd8e81840ca
Chris Eckhardt <ceckhardt06@gmail.com>
2020-02-17 18:42:07 -0800

    removed IDE specific files

--- application/flask_app/db/db_manager.py
+++ application/flask_app/db/db_manager.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from flask_app.db.config import db_conn
 
 ############################################
 #             Database Manager             #
