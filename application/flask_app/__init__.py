abeb5121705292151efe201d3711c2640e9c9763
Chris Eckhardt <ceckhardt06@gmail.com>
2020-02-17 18:48:34 -0800

    removed IDE specific files

--- application/flask_app/__init__.py
+++ application/flask_app/__init__.py
@@ -6,7 +6,7 @@ from flask_app.db.config import db_conn, track_mods
 import sqlalchemy.dialects.mysql.pymysql
 
 app = Flask(__name__)
-db_manager = DB_Manager()
-db_manager.init_db()
+#db_manager = DB_Manager()
+#db_manager.init_db()
 
 from flask_app import routes
