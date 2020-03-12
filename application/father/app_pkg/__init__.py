e03e2c9344781b71b03a44a156fc41eb87016107
Chris Eckhardt <ceckhardt06@gmail.com>
2020-03-12 12:39:19 -0700

    added search method handling and init'd db object

--- application/father/app_pkg/__init__.py
+++ application/father/app_pkg/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask import render_template
-from app_pkg.forms import SearchForm
+from flask import render_template, request
+from father.app_pkg.forms import SearchForm
+from father.database_manager.db_manager import DB
 
 
 app = Flask(__name__)
+# create DB object
+db = DB()
 
-@app.route('/')
-@app.route('/search')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+    # assign form
     form = SearchForm()
+    # determin HTTP method
+    if request.method == 'POST':
+        # query db
+        results = db.search_like(form['category'], form['term'])
+        return render_template('search.html', form=form, results=results)
     return render_template('search.html', form=form)
--- application/father/database_manager/db_manager.py
+++ application/father/database_manager/db_manager.py
@@ -1,6 +1,13 @@
 from father.database_manager import cnx
 
+
+
 class DB:
+
+
     def __init__(self):
         db_connection = cnx.MyDB()
+
+    def search_like(self, category, term):
+        return([{'test1', 'test1'}, {'test2', 'test2'}])
 
