8c265fa559659eb9efa2a0e58cb8842b366816ba
Chris Eckhardt <ceckhardt06@gmail.com>
2020-03-12 13:08:11 -0700

    added more digital_media categories' '

--- application/father/app_pkg/forms.py
+++ application/father/app_pkg/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class SearchForm(Form):
     term = StringField("Search", validators=[DataRequired()])
-    categories = [ ('all', 'all'), ('pictures', 'pictures'), ('videos', 'videos')]
+    categories = [ ('all', 'all'), ('picture', 'picture'), ('video', 'video'), ('audio', 'audio'), ('documents', 'documents')]
     category = SelectField(u'Category', choices = categories, validators = [DataRequired()])
     submit = SubmitField("Search")
