7e4c683080914c49dcd9605b7e2d9642cb316dc1
Akhil Gandu <akhilhello@gmail.com>
2020-03-12 00:02:59 -0700

    Installed application as a package with pip

--- application/database_manager/db_manager.py
+++ application/database_manager/db_manager.py
@@ -1,6 +1,17 @@
 import mysql.connector
+from application.config import db_conn
+from mysql.connector import errorcode
 
-cnx = mysql.connector.connect(user='root', password='apples',
-                              host='127.0.0.1',
-                              database='snapster')
-cnx.close()
+
+try:
+    cnx = mysql.connector.connect(user=db_conn['user'], password=db_conn['password'],
+                                  host=db_conn['host'], database=db_conn['database'])
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cnx.close()
