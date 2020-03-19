244b974b3dcce1b70c8a17e86e2fc31ca5f5a99a
Chris Eckhardt <ceckhardt06@gmail.com>
2020-03-18 21:18:02 -0700

    combined test db creator scripts, if the table exists drops table, create new table, fills table

--- application/father/test_pkg/db_M2_test_fill.py
+++ application/father/test_pkg/db_M2_test_fill.py
@@ -2,41 +2,4 @@ from father.database_manager import cnx
 
 db = cnx.MyDB()
 
-# this is just to clear all current entries to prevent duplicates of sponge bob images in database while testing
-#db.query("DELETE FROM digital_media_test WHERE owner_name='username001'")
-#db.commit()
 
-db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`)"
-"VALUES ("
-"'username001', 'sponge bob 1', 'this is the first test photo', '/M2_test_images/sb1.jpg', 'picture' , 99.99"
-")")
-db.commit()
-
-db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`)"
-"VALUES ("
-"'username001', 'sponge bob 2', 'this is the second test photo', '/M2_test_images/sb2.jpg', 'video' , 89.99"
-")")
-db.commit()
-
-db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`)"
-"VALUES ("
-"'username001', 'sponge bob 3', 'this is the third test photo', '/M2_test_images/sb3.jpg', 'document' , 79.99"
-")")
-db.commit()
-
-db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`)"
-"VALUES ("
-"'username001', 'sponge bob 4', 'this is the fourth test photo', '/M2_test_images/sb4.jpg', 'audio' , 69.99"
-")")
-db.commit()
-
-db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`)"
-"VALUES ("
-"'username001', 'sponge bob 5', 'this is the fifth test photo', '/M2_test_images/sb5.jpg', 'picture' , 59.99"
-")")
-db.commit()
--- application/father/test_pkg/db_create_test_table.py
+++ application/father/test_pkg/db_create_test_table.py
@@ -1,7 +1,21 @@
 from father.database_manager import cnx
 
+###############################################
+#
+# NOTE: This file replaces both previous
+#
+# 1. if the test table exists, it drops the table.
+# 2. create a new test table
+# 3. fill test table with data
+#
+##############################################3
+
 db = cnx.MyDB()
 
+# drop existing table
+db.query("DROP TABLE IF EXISTS digital_media_test")
+
+# create replacement table
 db.query("CREATE TABLE digital_media_test ("
          "media_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,"
          "owner_name VARCHAR(50),"
@@ -11,4 +25,41 @@ db.query("CREATE TABLE digital_media_test ("
          "thumbnail VARCHAR(200),"
          "category VARCHAR(20),"
          "price FLOAT(2),"
+         "approval INT,"
          "PRIMARY KEY (media_id))")
+
+# fill db
+db.query("INSERT INTO digital_media_test ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
+"VALUES ("
+"'username001', 'sponge bob 1', 'this is the first test photo', '/M2_test_images/sb1.jpg', 'picture' , 99.99, 0"
+")")
+db.commit()
+
+db.query("INSERT INTO digital_media_test ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
+"VALUES ("
+"'username001', 'sponge bob 2', 'this is the second test photo', '/M2_test_images/sb2.jpg', 'video' , 89.99, 0"
+")")
+db.commit()
+
+db.query("INSERT INTO digital_media_test ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
+"VALUES ("
+"'username001', 'sponge bob 3', 'this is the third test photo', '/M2_test_images/sb3.jpg', 'document' , 79.99, 0"
+")")
+db.commit()
+
+db.query("INSERT INTO digital_media_test ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
+"VALUES ("
+"'username001', 'sponge bob 4', 'this is the fourth test photo', '/M2_test_images/sb4.jpg', 'audio' , 69.99, 0"
+")")
+db.commit()
+
+db.query("INSERT INTO digital_media_test ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
+"VALUES ("
+"'username001', 'sponge bob 5', 'this is the fifth test photo', '/M2_test_images/sb5.jpg', 'picture' , 59.99, 0"
+")")
+db.commit()
