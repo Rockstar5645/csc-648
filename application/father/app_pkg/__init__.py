d3676dbde2338550a20337edbcec868f1ac46281
Chris Eckhardt <ceckhardt06@gmail.com>
2020-03-21 13:40:22 -0700

    moved BE_FreshStart to dev

---
+++ application/father/app_pkg/__init__.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from flask import render_template, request
+from father.app_pkg.team_data import team
+from father.database_manager.db_manager import DB
+from wtforms import Form
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, FileField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+# init flask application
+app = Flask(__name__)
+# create DB object
+db = DB()
+
+
+################################################
+#                    FORMS                     #
+################################################
+class SearchForm(Form):
+    term = StringField("Search", validators=[])
+    categories = db.get_category_select_field()
+    category = SelectField(u'Category', choices=categories, validators=[])
+    submit = SubmitField("Search")
+
+class RegistrationForm(Form):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(Form):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField('Login')
+
+################################################
+#                GENERAL ROUTING               #
+################################################
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    # assign form and results list
+    form = SearchForm()
+    results = []
+    # if : user submits POST request
+    if request.method == 'POST':
+        # query db
+        results = db.search(request.form['term'], request.form['category'])
+        # return results -------------------------------------vvv
+        return render_template('search_result.html', form=form, results=results)
+    # else : GET fresh html page
+    return render_template('search.html', form=form)
+
+@app.route("/about") 
+def about():
+    return render_template('about.html', team=team)
+
+
+
+##################################################
+#                TEAM MEMBER PAGES               #
+##################################################
+# NOTE: Defines team member "about" page routes
+
+@app.route("/avery")
+def avery():
+    return render_template("/about_team_member.html", team_member=db.get_team("Avery"))
+
+@app.route("/akhil")
+def akhil():
+    return render_template("/about_team_member.html", team_member=db.get_team("Akhil"))
+
+@app.route("/chris")
+def chris():
+    return render_template("/about_team_member.html", team_member=db.get_team("Chris"))
+
+@app.route("/elliot")
+def elliot():
+    return render_template("/about_team_member.html", team_member=db.get_team("Elliot"))
+
+@app.route("/thomas")
+def thomas():
+    return render_template("/about_team_member.html", team_member=db.get_team("Thomas"))
+
+@app.route("/bakulia")
+def bakulia():
+    return render_template("/about_team_member.html", team_member=db.get_team("Bakulia"))
---
+++ application/father/database_manager/db_manager.py
@@ -0,0 +1,62 @@
+from father.database_manager import cnx
+
+
+
+class DB:
+
+    def __init__(self):
+        self.db_connection = cnx.MyDB()
+
+    def search(self, term, category):
+        if term =='':
+            return self.get_category(category)
+        else:
+            return self.search_like(term, category)
+
+    def search_like(self, term, category):
+        if category == 'all':
+            self.db_connection.query(
+                "SELECT * "
+                "FROM digital_media_test "
+                "WHERE `name` LIKE %s OR `description` LIKE %s",
+                ("%" + term + "%","%" + term + "%",)
+            )
+        else:
+            self.db_connection.query(
+                "SELECT * "
+                "FROM digital_media_test "
+                "WHERE (`name` LIKE %s OR `description` LIKE %s) AND category LIKE %s",
+                ("%" + term + "%","%" + term + "%", "%" + category,)
+            )
+        data = self.db_connection.fetchall()
+        self.db_connection.commit()
+        if len(data) == 0:
+            data = self.get_category(category)
+        return data
+
+    def get_category(self, category):
+        self.db_connection.query("Select * FROM digital_media_test WHERE category LIKE %s", ("%" + category + "%",))
+        data = self.db_connection.fetchall()
+        self.db_connection.commit()
+        if len(data) == 0:
+            data = self.get_all_media()
+        return data
+    
+    def get_all_media(self):
+        self.db_connection.query("Select * FROM digital_media_test")
+        data = self.db_connection.fetchall()
+        self.db_connection.commit()
+        return data
+
+    def get_category_select_field(self):
+        self.db_connection.query("Select * FROM categories")
+        data = self.db_connection.fetchall()
+        self.db_connection.commit()
+        cats = [(c[1], c[1]) for c in data]
+        return cats
+
+    def get_team(self, name):
+        self.db_connection.query("Select * FROM team_about WHERE `name` %s", ("%"+name+"%",))
+        data = self.db_connection.fetchall()
+        self.db_connection.commit()
+        return data
