f70de5e975a696e7b5f90e83a87940df2efe628d
Chris Eckhardt <ceckhardt06@gmail.com>
2020-03-21 18:38:36 -0700

    added categories table to createTestDb.py

--- application/father/test_pkg/createTestDB.py
+++ application/father/test_pkg/createTestDB.py
@@ -21,6 +21,9 @@ db = cnx.MyDB()
 ###############################
 db.query("DROP TABLE IF EXISTS digital_media_test")
 db.query("DROP TABLE IF EXISTS team_about")
+db.query("DROP TABLE IF EXISTS categories")
+# db.query("DROP TABLE IF EXISTS team_about")
+
 
 #######################################
 #      DIGITAL MEDIA TEST TABLE       #
@@ -42,40 +45,89 @@ db.query("CREATE TABLE digital_media_test ("
 #       FILL DIGITAL MEDIA TEST       #
 #######################################
 db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
-"VALUES ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`"
+") VALUES ("
 "'username001', 'sponge bob 1', 'this is the first test photo', '/M2_test_images/sb1.jpg', 'image' , 99.99, 0"
 ")")
 db.commit()
 
 db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
-"VALUES ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`"
+") VALUES ("
 "'username001', 'sponge bob 2', 'this is the second test photo', '/M2_test_images/sb2.jpg', 'video' , 89.99, 0"
 ")")
 db.commit()
 
 db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
-"VALUES ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`"
+") VALUES ("
 "'username001', 'sponge bob 3', 'this is the third test photo', '/M2_test_images/sb3.jpg', 'document' , 79.99, 0"
 ")")
 db.commit()
 
 db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
-"VALUES ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`"
+") VALUES ("
 "'username001', 'sponge bob 4', 'this is the fourth test photo', '/M2_test_images/sb4.jpg', 'audio' , 69.99, 0"
 ")")
 db.commit()
 
 db.query("INSERT INTO digital_media_test ("
-"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`)"
-"VALUES ("
+"`owner_name`, `name`, `description`, `file_path`, `category`, `price`, `approval`"
+") VALUES ("
 "'username001', 'sponge bob 5', 'this is the fifth test photo', '/M2_test_images/sb5.jpg', 'image' , 59.99, 0"
 ")")
 db.commit()
 
+###############################
+#      CATEGORIES TABLE       #
+###############################
+
+db.query("CREATE TABLE IF NOT EXISTS categories ("
+         "cat_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,"
+         "category VARCHAR(50),"
+         "PRIMARY KEY (cat_id))")
+
+############################
+#      FILL CATEGORIES     #
+############################
+
+db.query("INSERT INTO categories ("
+"`category`)"
+"VALUES ("
+"'all'"
+")")
+db.commit()
+
+db.query("INSERT INTO categories ("
+"`category`)"
+"VALUES ("
+"'image'"
+")")
+db.commit()
+
+db.query("INSERT INTO categories ("
+"`category`)"
+"VALUES ("
+"'video'"
+")")
+db.commit()
+
+db.query("INSERT INTO categories ("
+"`category`)"
+"VALUES ("
+"'audio'"
+")")
+db.commit()
+
+db.query("INSERT INTO categories ("
+"`category`)"
+"VALUES ("
+"'document'"
+")")
+db.commit()
+
+'''
 ###############################
 #      TEAM ABOUT TABLE       #
 ###############################
@@ -99,7 +151,7 @@ db.query("CREATE TABLE IF NOT EXISTS team_about ("
 
 # Avery
 db.query("INSERT INTO team_about ("
-        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`)"
+        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`),"
         "VALUES ("
         "'Avery Chen', '/avery', 'Back-end Member', 'static/team_images/avery.jpg', 'Undergraduate student at SFSU', '', '', '', ''"
         ")")
@@ -107,7 +159,7 @@ db.commit()
 
 # Akhil
 db.query("INSERT INTO team_about ("
-        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`)"
+        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`),"
         "VALUES ("
         "'Akhil Gandu', '/akhil', 'GitHub Master', 'static/team_images/akhil.jpg', 'Graduate Student at SFSU', 'https://www.facebook.com/rockstar290', 'https://twitter.com/Rockstar5645', 'https://www.instagram.com/akhil_gandu/', 'https://www.linkedin.com/in/akhilgandu/'"
         ")")
@@ -115,7 +167,7 @@ db.commit()
 
 # Chris
 db.query("INSERT INTO team_about ("
-        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`)"
+        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`),"
         "VALUES ("
         "'Chris Eckhardt', '/chris', 'Back-end lead', 'static/team_images/chris.jpg', 'Undergrad @ SFState', '', '', 'https://www.instagram.com/chris_evan_eckhardt/', 'https://www.linkedin.com/in/christopher-eckhardt-04abb1119/'"
         ")")
@@ -123,15 +175,15 @@ db.commit()
 
 # Elliot
 db.query("INSERT INTO team_about ("
-        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`)"
+        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`),"
         "VALUES ("
-        "'Elliot Yardley', '/elliot', 'Front-end lead', 'static/team_images/elliot.jpg', 'Graduate Student, SFSU', '', '', '', ''"
+        "'Elliot Yardley', '/elliot', 'Front-end lead', 'static/team_images/elliot.jpg', 'Graduate Student, SFSU'', '', '', '', ''"
         ")")
 db.commit()
 
 # Thomas
 db.query("INSERT INTO team_about ("
-        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`)"
+        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`),"
         "VALUES ("
         "'Thomas Yu', '/thomas', 'Front End Member', 'static/team_images/Thomas.jpg', 'Undergraduate Student, SFSU', '', '', '', ''"
         ")")
@@ -139,8 +191,9 @@ db.commit()
 
 #Bakulia
 db.query("INSERT INTO team_about ("
-        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`)"
+        "`name`, `position`, `link`, `image`, `description`, `facebook`, `twitter`, `instagram`, `linkedin`),"
         "VALUES ("
         "'Bakulia Kurmant', '/bakulia', 'Team Lead', 'static/team_images/bakulia.jpg', 'In my spare time I like going outdoors, techno music, reading, traveling', '', '', 'https://www.instagram.com/bakuliak/?hl=en', 'https://www.linkedin.com/in/bakulia-kurmant/'"
         ")")
 db.commit()
+'''
