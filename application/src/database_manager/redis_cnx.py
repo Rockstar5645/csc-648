11dfd9081bbc5305a1b42dacea244cd9ec6290be
Akhil Gandu <akhilhello@gmail.com>
2020-04-08 19:27:46 -0700

    updated redis connection paramaters to make password optional

--- application/src/database_manager/redis_cnx.py
+++ application/src/database_manager/redis_cnx.py
@@ -3,7 +3,10 @@ from src.config import redis_conn
 
 def get_redis_con():
     if redis_conn['mode'] == 'prod':
-        return redis.Redis(host=redis_conn['host'], port=redis_conn['port'], password=redis_conn['password'])
+        if 'password' in redis_conn:
+            return redis.Redis(host=redis_conn['host'], port=redis_conn['port'], password=redis_conn['password'])
+        else:
+            return redis.Redis(host=redis_conn['host'], port=redis_conn['port'])
     else:
         rco = MyRLocal()
         return rco
