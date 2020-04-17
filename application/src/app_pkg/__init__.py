991610ec41cbf6701569647aa4d019d6562f6110
Akhil Gandu <akhilhello@gmail.com>
2020-04-17 15:15:31 -0700

    moved all application initialization code into a seperate initializer package

--- application/src/app_pkg/__init__.py
+++ application/src/app_pkg/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from src.database_manager.db_manager import DB
-from flask_login import (LoginManager, login_user, 
-                         current_user, logout_user)
-from itsdangerous import URLSafeTimedSerializer
-
-
+from flask_login import (LoginManager)
 
 # init flask application
 app = Flask(__name__)
@@ -18,8 +14,7 @@ login_manager = LoginManager()
 #Tell the login manager where to redirect users to display the login page
 login_manager.login_view = "/login"
 #Setup the login manager. 
-login_manager.setup_app(app) 
-from src.database_manager.objects import user
+login_manager.setup_app(app)
 
 # import routes
 from src.app_pkg import routes
