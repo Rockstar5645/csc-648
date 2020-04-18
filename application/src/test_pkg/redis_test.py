4c2820c947303ca5745015f597b4a6ba63728684
Akhil Gandu <akhilhello@gmail.com>
2020-04-17 23:06:58 -0700

    I forgot what I did here

--- application/src/test_pkg/redis_test.py
+++ application/src/test_pkg/redis_test.py
@@ -1,4 +1,5 @@
 import redis
+import time
 
 local_redis = {
     'host': '10.0.0.188',
@@ -9,10 +10,13 @@ local_redis = {
 r = redis.Redis(**local_redis)
 
 def set_value():
-    if (r.setex('abc', 60, 30)):
+    if (r.setex('abc', 3, 3)):
         print('successfully set the value')
     else:
         print('unsuccessful')
+    print(r.get('abc'))
+    print(r.ttl('abc'))
+    print(r.exists('abc'))
 
 def get_value():
 
@@ -20,17 +24,25 @@ def get_value():
         with r.pipeline() as pipe:
             pipe.multi()
             pipe.get('abc')
-            pipe.expire('abc', 60)
+            pipe.expire('abc', 2)
             gotten_val = pipe.execute()
 
-        if (gotten_val[1]):
-            print('value of {} was successfully set'.format(gotten_val[0].decode('utf-8')))
-        else:
-            print('we were not able to update the value successfully')
+        print('value retrieved from redis server')
+        print(gotten_val)
+        print(r.get('abc'))
+        print(r.ttl('abc'))
+        print(r.exists('abc'))
+
+        if (gotten_val[0] is None):
+            print('The value of abc is no longer on the server')
+
+        if (r.exists('abc') == 0):
+            print('Checked existence, no longer there')
 
     except redis.exceptions.TimeoutError as err:
         print('Redis connection error: {}'.format(err))
 
-# set_value()
+set_value()
 get_value()
-
+time.sleep(3)
+get_value()
