0ca13e0db94194743da361563d38b31f7d1047f5
Akhil Gandu <akhilhello@gmail.com>
2020-04-20 15:53:55 -0700

    simplified the login and register api

--- application/src/database_manager/register.py
+++ application/src/database_manager/register.py
@@ -14,9 +14,13 @@ def register(username, email, password, db, r):
             return session_msg
         elif session_msg['status'] == 'redis_error':
             # the user was added to the database, but we weren't able to generate a valid session token
-            session_msg['status'] == 'added_user'
-            return session_msg
+            err_msg = {
+                'status': 'user-added'
+            }
+            return err_msg
     elif status_msg['status'] == 'database_error':
-        status_msg['status'] = 'error'
-        return status_msg
+        err_msg = {
+            'status': 'fail'
+        }
+        return err_msg
 
