db1ce1ad5172a5ed4ce105de1b87623bc91d944d
Chris Eckhardt <ceckhardt06@gmail.com>
2020-05-04 17:03:59 -0700

    added more entries to db test fill, to test pagination

--- application/src/app_pkg/routes/login.py
+++ application/src/app_pkg/routes/login.py
@@ -1,7 +1,5 @@
-from flask import render_template, request
 from src.app_pkg.routes.common import validate_helper
-from src.app_pkg import app
-from src.app_pkg import db
+from src.app_pkg import app, db
 from src.app_pkg.forms import LoginForm
 from flask import render_template, request, redirect, url_for, make_response
 
@@ -28,4 +26,4 @@ def login():
             res.set_cookie('token', 'none')
             return res
     else:
-        return render_template('login.html', form=form)
+        return render_template('login.html', form=form, isloggedin=isloggedin)
---
+++ application/src/app_pkg/routes/message.py
@@ -0,0 +1,15 @@
+from flask import render_template, request
+from src.app_pkg.routes.common import validate_helper
+from src.app_pkg import app, db
+from src.app_pkg.forms import MessageForm
+
+@app.route('/message', method=['GET', 'POST'])
+def message():
+    isloggedin = validate_helper(request.cookies)
+    form = MessageForm()
+    if request.method == 'POST':
+        pass
+        # get message data
+        # call db
+        # return to parent url (if this is a modal)
+
