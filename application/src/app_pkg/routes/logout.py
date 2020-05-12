13635bb7d528f27ee733b377e9de97f46037cb11
Chris Eckhardt <ceckhardt06@gmail.com>
2020-05-12 15:31:15 -0700

    fixed typo in logout.py

--- application/src/app_pkg/routes/logout.py
+++ application/src/app_pkg/routes/logout.py
@@ -2,8 +2,8 @@ from src.app_pkg.routes.common import validate_helper
 from src.app_pkg import app, db
 from flask import render_template, request, redirect, url_for, make_response
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
     isloggedin = validate_helper(request.cookies)
     if isloggedin:
         db.logout(request.cookies['token'])
