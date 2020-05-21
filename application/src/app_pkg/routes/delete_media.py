65e044de764429fc47f42e02e80b0e9310dccae6
Avery Chen <averychen2122@yahoo.com>
2020-05-20 19:11:59 -0700

    added changes to delete function, not hooked to frontend yet

--- application/src/app_pkg/routes/delete_media.py
+++ application/src/app_pkg/routes/delete_media.py
@@ -1,24 +1,17 @@
 import os
-from PIL import Image as PILImage
-from tkinter import Image
 from src.app_pkg import app, db
-from src.app_pkg.routes.common import validate_helper
-from src.app_pkg.forms import SubmissionForm
-from src.app_pkg.forms import SearchForm
-from flask import render_template, request, redirect, url_for, make_response, flash, send_from_directory
-from werkzeug.utils import secure_filename
+from flask import render_template, request, redirect, url_for
 from src.app_pkg.objects.user import User
 
 ##################################################
 #                DELETE MEDIA                    #
 ##################################################
-
+# not tested yet
 @app.route('/delete', methods=['GET', 'POST'])
 def delete_file():
     user = User(request.cookies)
-
-    file = request.form['']     # get file path or file name
-    os.remove(file)             # remove file from static folder
-    db.delete_file()            # delete file from db, pass args to function
-
-    return render_template()
+    media_id = request.form['media-id']
+    media_path = user.get_digital_media_path_by_id(media_id)
+    os.remove(media_path)
+    db.delete_file(media_id)
+    return redirect(url_for('search'))
